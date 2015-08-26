706fe03b252d5feb55fe5b425354ed99cde526f1
Matt Ball <matt.ball.2@gmail.com>
2015-08-26 00:06:18 +0000

    adds run_server script and install reqs

--- setup.py
+++ setup.py
@@ -16,12 +16,26 @@ setup(
   description='eno test client',
   long_description=README,
   url='http://github.com/endaga/eno-python',
-  download_url=('https://github.com/endaga/eno-python/tarball/%s' %
-                VERSION),
+  download_url='https://github.com/endaga/eno-python/tarball/%s' % VERSION,
   author='Matt Ball',
   author_email='matt@endaga.com',
   license='MIT',
   packages=['eno'],
-  install_requires=[],
-  zip_safe=False
+  install_requires=[
+    "Adafruit-BBIO==0.0.30",
+    "Flask==0.10.1",
+    "Jinja2==2.8",
+    "MarkupSafe==0.23",
+    "PyYAML==3.11",
+    "Werkzeug==0.10.4",
+    "argparse==1.2.1",
+    "itsdangerous==0.24",
+    "pyserial==2.7",
+    "python-gsmmodem==0.9",
+    "wsgiref==0.1.2",
+  ],
+  zip_safe=False,
+  scripts=[
+    'scripts/run_server.py',
+  ]
 )
